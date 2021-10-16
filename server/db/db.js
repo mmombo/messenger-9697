39d2e126bb75cfcf17a9236fc4cf667bfad6bcb7
Matt Mombourquette <mmombou@uwo.ca>
2021-10-15 20:51:56 -0400

    updated db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,11 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+const db = new Sequelize(process.env.DB_NAME, process.env.PG_USER, process.env.PG_PASS, {
+  host: "localhost",
+  port: "5432",
+  dialect: "postgres",
+  logging: false,
 });
 
 module.exports = db;
