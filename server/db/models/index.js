a65ca4a5733a1d16075a345e0335de65f4ec1578
Matt Mombourquette <mmombou@uwo.ca>
2021-10-19 20:28:50 -0400

    changes to models for issue 3

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,15 +3,13 @@ const User = require("./user");
 const Message = require("./message");
 
 // associations
-
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.belongsToMany(Conversation, { through: "User_Conversation" });
+Conversation.belongsToMany(User, { through: "User_Conversation" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
 };
